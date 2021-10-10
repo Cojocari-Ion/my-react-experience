850b04a2a5e49589d95d043d32a1aef045d78beb
Cojocari-Ion <69515189+Cojocari-Ion@users.noreply.github.com>
2021-10-10 09:41:21 +0300

    ok

--- experience-app/src/components/Portfolio/Portfolio.js
+++ experience-app/src/components/Portfolio/Portfolio.js
@@ -9,49 +9,49 @@ const Portfolio =()=> {
             id: 0,
             tittle: "MyFood",
             iamge: "imgs/myfood.png",
-            link: ""
+            link: "https://github.com/Cojocari-Ion/MyFood"
         },
 
         {
             id: 1,
             tittle: "Blueline",
             iamge: "imgs/blueline.png",
-            link: ""
+            link: "https://github.com/Cojocari-Ion/BlueLine"
         },
 
         {
             id: 2,
             tittle: "MEDfamily",
             iamge: "imgs/medfamily.png",
-            link: ""
+            link: "https://github.com/Cojocari-Ion/MedFamily"
         },
 
         {
             id: 3,
             tittle: "Tours App",
             iamge: "imgs/tours.png",
-            link: ""
+            link: "https://github.com/Cojocari-Ion/tours-react-"
         },
 
         {
             id: 4,
             tittle: "Recipes App",
             iamge: "imgs/recipes.png",
-            link: ""
+            link: "https://github.com/Cojocari-Ion/recipe-app-react-/tree/main/my-app"
         },
 
         {
             id: 5,
             tittle: "Breacking Bad App",
             iamge: "imgs/breacking-bad.png",
-            link: ""
+            link: "https://github.com/Cojocari-Ion/breacking-bad-react"
         },
 
         {
             id: 6,
             tittle: "My CV App",
             iamge: "imgs/myCV.png",
-            link: ""
+            link: "https://github.com/Cojocari-Ion/my-react-experience/tree/main/experience-app"
         },
     ]
 
